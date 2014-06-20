b010a47a1f508911cddf2c7dd9c9873623bf5b5b
Bessie <songy@cobalt.com>
2014-06-20 15:36:25 -0700

    Add a webpage

--- src/main/java/com/cobalt/cdpipeline/servlet/MainPage.java
+++ src/main/java/com/cobalt/cdpipeline/servlet/MainPage.java
@@ -9,14 +9,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.net.URI;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+
+import com.atlassian.templaterenderer.TemplateRenderer;
+
 public class MainPage extends HttpServlet{
     private static final Logger log = LoggerFactory.getLogger(MainPage.class);
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    private final UserManager userManager;
+    private final LoginUriProvider loginUriProvider;
+    private final TemplateRenderer renderer;
+   
+    public MainPage(UserManager userManager, LoginUriProvider loginUriProvider,  TemplateRenderer renderer)
     {
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World</body></html>");
+      this.userManager = userManager;
+      this.loginUriProvider = loginUriProvider;
+      this.renderer = renderer;
+    }
+   
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+      String username = userManager.getRemoteUsername(request);
+      if (username == null || !userManager.isSystemAdmin(username))
+      {
+        redirectToLogin(request, response);
+        return;
+      }
+      
+      response.setContentType("text/html;charset=utf-8");
+      renderer.render("cdpipeline.vm", response.getWriter());
+    }
+    
+    private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+      response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+    }
+      
+    private URI getUri(HttpServletRequest request)
+    {
+      StringBuffer builder = request.getRequestURL();
+      if (request.getQueryString() != null)
+      {
+        builder.append("?");
+        builder.append(request.getQueryString());
+      }
+      return URI.create(builder.toString());
     }
 
 }
