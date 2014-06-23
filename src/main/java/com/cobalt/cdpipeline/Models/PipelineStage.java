3ad32004abb83c5c4b8cffa911ddf2c91c969761
ArielLin <lintz@cobalt.com>
2014-06-23 16:52:38 -0700

    Added javadoc to Contributor and PipelineStage

--- src/main/java/com/cobalt/cdpipeline/Models/PipelineStage.java
+++ src/main/java/com/cobalt/cdpipeline/Models/PipelineStage.java
@@ -1,7 +1,10 @@
 package com.cobalt.cdpipeline.Models;
 
+/**
+ * 
+ *
+ */
 public class PipelineStage {
-	// states
 	public static final int FAILED = 0;
 	public static final int UNKNOWN = 1;
 	public static final int SUCCESSFUL = 2;
@@ -9,21 +12,37 @@ public class PipelineStage {
 	private String stageName;
 	private int state;
 	
+	/**
+	 * Constructs a PipelineStage object.
+	 * 
+	 * @param stageName name of this pipeline stage
+	 * @param state state of this pipeline stage in the build
+	 */
 	public PipelineStage(String stageName, String state) {
 		this.stageName = stageName;
 		
 		if (state.equals("Failed"))
 			this.state = PipelineStage.FAILED;
-		else if (state.equals("Unknown"))
-			this.state = PipelineStage.UNKNOWN;
-		else
+		else if (state.equals("Successful"))
 			this.state = PipelineStage.SUCCESSFUL;
+		else
+			this.state = PipelineStage.UNKNOWN;
 	}
 	
+	/**
+	 * Gets the stage name of this pipeline stage
+	 * 
+	 * @return the stageName
+	 */
 	public String getStageName() {
 		return stageName;
 	}
 	
+	/**
+	 * Gets the state of this pipeline stage.
+	 * 
+	 * @return the state, which can be FAILED, UNKNOWN, or SUCCESSFUL.
+	 */
 	public int getState() {
 		return state;
 	}
