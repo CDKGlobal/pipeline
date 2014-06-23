2b876e247ee25faf572f27e3f4817a2b317cf83a
liuch <liuch@cobalt.com>
2014-06-23 16:23:46 -0700

    Added Plan object

--- src/main/java/com/cobalt/cdpipeline/Models/Plan.java
+++ src/main/java/com/cobalt/cdpipeline/Models/Plan.java
@@ -1,5 +1,53 @@
 package com.cobalt.cdpipeline.Models;
 
-public class Plan {
+import java.util.Date;
 
+/**
+ * 
+ */
+public class Plan {
+	private String projectName, planName, planKey;
+	public Date completedTime, lastUpdate;
+	public int changes;
+	
+	public Contributor[] contributors;
+	public CurrentBuild currentBuild;
+	public PipelineStage[] PipelineStages; 
+	
+	/**
+	 * Construct a Plan object 
+	 * @param projectName
+	 * @param planName
+	 * @param planKey
+	 */
+	public Plan (String projectName, String planName, String planKey) {
+		this.projectName = projectName;
+		this.planName = planName;
+		this.planKey = planKey;
+	}
+	
+	/**
+	 * Get project name
+	 * @return
+	 */
+	public String getProjectName() {
+		return projectName;
+	}
+	
+	/**
+	 * Get plan name
+	 * @return
+	 */
+	public String getPlanName() {
+		return planName;
+	}
+	
+	/**
+	 * Get plan key
+	 * @return
+	 */
+	public String getPlanKey() {
+		return planKey;
+	}
+	
 }
