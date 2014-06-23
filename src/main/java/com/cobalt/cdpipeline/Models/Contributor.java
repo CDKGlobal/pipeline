11330c6895f5db926779580e45fb9b0e3e330774
ArielLin <lintz@cobalt.com>
2014-06-23 16:52:38 -0700

    Added javadoc to Contributor and PipelineStage

--- src/main/java/com/cobalt/cdpipeline/Models/Contributor.java
+++ src/main/java/com/cobalt/cdpipeline/Models/Contributor.java
@@ -1,12 +1,24 @@
 package com.cobalt.cdpipeline.Models;
 
+/**
+ * 
+ */
 public class Contributor {
 	private String username;
 	
+	/**
+	 * Constructs a Contributor object.
+	 * 
+	 * @param username of the contributor
+	 */
 	public Contributor(String username) {
 		this.username = username;
 	}
 	
+	/**
+	 * Gets the username of this.
+	 * @return the username
+	 */
 	public String getUsername() {
 		return username;
 	}
