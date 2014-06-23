67e9e24bd6940aaa69bd74de7363ac35c4fba545
Bessie <songy@cobalt.com>
2014-06-23 16:56:47 -0700

    Add JavaDoc to Build Object

--- src/main/java/com/cobalt/cdpipeline/Models/Build.java
+++ src/main/java/com/cobalt/cdpipeline/Models/Build.java
@@ -7,20 +7,38 @@ public class Build {
 	private int buildNumber;
 	private Date completeTime;
 	
+	/**
+	 * Constructs a Build with given params
+	 * @param buildKey
+	 * @param buildNumber
+	 * @param completeTime
+	 */
 	public Build(String buildKey, int buildNumber, Date completeTime){
 		this.buildKey = buildKey;
 		this.buildNumber = buildNumber;
 		this.completeTime = completeTime;
 	}
 
+	/**
+	 * Returns the build key of this build
+	 * @return build key
+	 */
 	public String getBuildKey() {
 		return buildKey;
 	}
 
+	/**
+	 * Returns the build number of this build
+	 * @return build number
+	 */
 	public int getBuildNumber() {
 		return buildNumber;
 	}
 
+	/**
+	 * Returns the completed time of this build
+	 * @return completed time
+	 */
 	public Date getCompleteTime() {
 		return completeTime;
 	}
