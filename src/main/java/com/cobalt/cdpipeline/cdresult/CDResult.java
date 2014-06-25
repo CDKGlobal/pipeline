e5dde8987ec14d73c4d8b97ae19ade7e7e6932b3
ArielLin <lintz@cobalt.com>
2014-06-25 12:11:21 -0700

    Added setters for all fields in CDResult.

--- src/main/java/com/cobalt/cdpipeline/cdresult/CDResult.java
+++ src/main/java/com/cobalt/cdpipeline/cdresult/CDResult.java
@@ -2,19 +2,21 @@ package com.cobalt.cdpipeline.cdresult;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 
  */
 public class CDResult {
 	private String projectName, planName;
-	public Date completedTime, lastUpdate;
-	public int changes;
+	private Date lastDeploymentTime, lastUpdate;
+	private int numChanges;
 	
-	public List<Contributor> contributors;
-	public Build currentBuild;
-	public List<PipelineStage> pipelineStages; 
+	private Set<Contributor> contributors;
+	private Build currentBuild;
+	private List<PipelineStage> pipelineStages; 
 	
 	/**
 	 * Construct a Plan object 
@@ -22,12 +24,62 @@ public class CDResult {
 	 * @param planName
 	 * @param planKey
 	 */
-	public CDResult (String projectName, String planName) {
-		contributors = new ArrayList<Contributor>();
-		pipelineStages = new ArrayList<PipelineStage>();
-
+	public CDResult(String projectName, String planName) {
 		this.projectName = projectName;
 		this.planName = planName;
+		
+		contributors = new HashSet<Contributor>();
+		pipelineStages = new ArrayList<PipelineStage>();
+	}
+	
+	/**
+	 * Set lastDeploymentTime to the given date.
+	 * Scope identifier left out intentionally for package protection.
+	 * 
+	 * @param lastDeployment
+	 */
+	void setLastDeploymentTime(Date lastDeployment) {
+		this.lastDeploymentTime = lastDeployment;
+	}
+	
+	/**
+	 * Set lastUpdate time to the given date.
+	 * Scope identifier left out intentionally for package protection.
+	 * 
+	 * @param lastUpdate
+	 */
+	void setLastUpdateTime(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	
+	/**
+	 * Set number of changes to the given number.
+	 * Scope identifier left out intentionally for package protection.
+	 * 
+	 * @param numChanges
+	 */
+	void setNumChanges(int numChanges) {
+		this.numChanges = numChanges;
+	}
+	
+	/**
+	 * Add given contributor to contributors set.
+	 * Scope identifier left out intentionally for package protection.
+	 * 
+	 * @param contributor
+	 */
+	void addContributorToSet(Contributor contributor) {
+		contributors.add(contributor);
+	}
+	
+	/**
+	 * Add given PipelineStage to the pipelineStages list.
+	 * Scope identifier left out intentionally for package protection.
+	 * 
+	 * @param stage
+	 */
+	void addPipelineStageToList(PipelineStage stage) {
+		pipelineStages.add(stage);
 	}
 	
 	/**
