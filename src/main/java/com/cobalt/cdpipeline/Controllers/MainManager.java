aa892a6b498d247dfc57e039c178fa82a22afc13
ArielLin <lintz@cobalt.com>
2014-06-26 09:23:56 -0700

    Added comments to MainManager

--- src/main/java/com/cobalt/cdpipeline/Controllers/MainManager.java
+++ src/main/java/com/cobalt/cdpipeline/Controllers/MainManager.java
@@ -14,11 +14,23 @@ import com.atlassian.bamboo.resultsummary.ResultsSummaryManager;
 import com.cobalt.cdpipeline.cdresult.CDResult;
 import com.cobalt.cdpipeline.cdresult.CDResultFactory;
 
+/**
+ * The main controller of CDPipeline Plugin Project that handles getting the
+ * results needed for displaying the table.
+ */
 public class MainManager {
 	private ProjectManager projectManager;
 	private PlanManager planManager;
 	private ResultsSummaryManager resultsSummaryManager;
 	
+	/**
+	 * Constructs a MainManager object.
+	 * 
+	 * @param projectManager The ProjectManager (within Bamboo) to get information about projects.
+	 * @param planManager The PlanMananger (within Bamboo) to get information about plans.
+	 * @param resultsSummaryManager The ResultsSummaryManager (within Bamboo) to get information
+	 *                              about builds.
+	 */
 	public MainManager(ProjectManager projectManager, PlanManager planManager, 
 			                       ResultsSummaryManager resultsSummaryManager) {
 		this.projectManager = projectManager;
@@ -26,6 +38,12 @@ public class MainManager {
 		this.resultsSummaryManager = resultsSummaryManager;
 	}
 	
+	/**
+	 * Get all the results needed for displaying the CDPipeline table.
+	 * 
+	 * @return a list of CDResults, where each CDResult represents one row.
+	 *         See CDResults for more details.
+	 */
 	public List<CDResult> getCDResults() {
 		List<CDResult> resultList = new ArrayList<CDResult>();
 		
@@ -36,9 +54,9 @@ public class MainManager {
 			List<TopLevelPlan> plans = planManager.getAllPlansByProject(project, TopLevelPlan.class);
 			for (Plan plan : plans) {
 				String planName = plan.getName();
-				List<ResultsSummary> resultSummaryList = resultsSummaryManager.getResultSummariesForPlan(plan, 0, 0);
+				List<ResultsSummary> buildList = resultsSummaryManager.getResultSummariesForPlan(plan, 0, 0);
 				
-				CDResultFactory factory = new CDResultFactory(projectName, planName, resultSummaryList);
+				CDResultFactory factory = new CDResultFactory(projectName, planName, buildList);
 				CDResult result = factory.createCDResult();
 				resultList.add(result);
 			}
