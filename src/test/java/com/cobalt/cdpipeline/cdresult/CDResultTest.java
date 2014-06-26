bff4d178c33de8d2c345d0c7c72dd6afeaaca09f
Bessie <songy@cobalt.com>
2014-06-26 15:28:27 -0700

    Create a folder to store all tests.

---
+++ src/test/java/com/cobalt/cdpipeline/cdresult/CDResultTest.java
@@ -0,0 +1,13 @@
+package com.cobalt.cdpipeline.cdresult;
+
+import org.junit.Test;
+
+
+public class CDResultTest {
+
+	@Test
+	public void testGetDaysSinceDeploymentFromCurrent() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
