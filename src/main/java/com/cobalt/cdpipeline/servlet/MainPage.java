9bab7032e8a29789a0273d0128dddcf0c35ad02a
wilbert <lamwi@cobalt.com>
2014-06-26 13:31:04 -0700

    displays table data passed from back end and formatted

--- src/main/java/com/cobalt/cdpipeline/servlet/MainPage.java
+++ src/main/java/com/cobalt/cdpipeline/servlet/MainPage.java
@@ -1,5 +1,9 @@
 package com.cobalt.cdpipeline.servlet;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
+import com.google.common.collect.Maps;
 
 import com.atlassian.bamboo.plan.PlanManager;
 import com.atlassian.bamboo.project.ProjectManager;
@@ -46,9 +53,29 @@ public class MainPage extends HttpServlet{
         redirectToLogin(request, response);
         return;
       }
+//      VelocityEngine velocity = new VelocityEngine();
+//      velocity.init();
       
+     
+//      Template temp = velocity.getTemplate("cdpipeline.vm");
+//      
+//      VelocityContext context = new VelocityContext();
+//      context.put("results", resultList);
+//      StringWriter writer = new StringWriter();
+//      temp.merge(context, writer);
+//      System.out.println(writer);
+      
+      
+   // Create the Velocity Context
+//      Map context = MacroUtils.defaultVelocityContext();
+//      context.put("results", resultList);
+//      // Render the Template
+//      String result = VelocityUtils.getRenderedTemplate("cdpipeline.vm", context);
+      List<CDResult> resultList = mainManager.getCDResults();
+      Map<String, Object> context =  Maps.newHashMap();
+      context.put("results", resultList);
       response.setContentType("text/html;charset=utf-8");
-      renderer.render("cdpipeline.vm", response.getWriter());
+      renderer.render("cdpipeline.vm", context, response.getWriter());
     }
     
     private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException
