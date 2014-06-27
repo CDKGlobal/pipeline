480156fcb05698b04b973bf62ac4a281abb30055
ArielLin <lintz@cobalt.com>
2014-06-27 09:42:29 -0700

    Added tests for MainManagerTest.

--- src/test/java/com/cobalt/cdpipeline/Controllers/MainManagerTest.java
+++ src/test/java/com/cobalt/cdpipeline/Controllers/MainManagerTest.java
@@ -36,59 +36,115 @@ public class MainManagerTest {
         main = new MainManager(null, null, null);
         List<CDResult> results = main.getCDResults();
     }
-
-    /*
-    @Test
-    public void testDataIsWhatWeExpect() {
-        Project project1 = mock(Project.class);
-        Project project2 = mock(Project.class);
-
-        when(project1.getName()).thenReturn("Project 1");
-        when(project2.getName()).thenReturn("Project 2");
-
-        Set<Project> projects = new HashSet<Project>();
-        projects.add(project1);
-        projects.add(project2);
-
-        ProjectManager manager = mock(ProjectManager.class);
-        when(manager.getAllProjects()).thenReturn(projects);
-
-        //MainManager cut = ;
-        int expectedCount = 2;
-        int actualCount = manager.getAllProjects().size();
-        assertEquals("The count was not what we expected", expectedCount, actualCount);
-    }
-    */
     
+    // The following tests only test the project-and-plan logic in
+    // GetCDResults using the size of the result list. 
+    // Empty build lists are used as the mock up data.
+    
+    // no project
     @Test
-    public void testGetCDResultsWithSmallSetAndEmptyBuildListBySize() {
-    	Set<Project> projects = createSmallProjectSet();
-    	createProjectManager(projects);
-    	Map<Project, List<TopLevelPlan>> map = new HashMap<Project, List<TopLevelPlan>>();
-    	for (Project pj : projects) {
-    		List<TopLevelPlan> plans = createSmallPlanList(pj);
-    		map.put(pj, plans);
-    	}
-    	createPlanManager(projects, map);
-    	createEmptyResultsSummaryManager(map);
-    	main = new MainManager(projectMgr, planMgr, resultsSumMgr);
-    	assertEquals("The count of CDResult list does not match", 4, main.getCDResults().size());
+    public void testGetCDResultWithNoProjectBySize() {
+    	testWithEmptyBuildListBySize(0, 0, 0);
+    }
+    
+    // 1 project, no plans
+    @Test
+    public void testGetCDResultWithOneProjectNoPlanBySize() {
+    	testWithEmptyBuildListBySize(1, 0, 0);
+    }
+    
+    // 3 projects, each with no plans
+    @Test
+    public void testGetCDResultWithSmallProjectSetAndNoPlansBySize() {
+    	testWithEmptyBuildListBySize(3, 0, 0);
+    }
+    
+    // 20 projects, each with no plans
+    @Test
+    public void testGetCDResultWithBiggerProjectSetAndNoPlansBySize() {
+    	testWithEmptyBuildListBySize(20, 0, 0);
+    }
+    
+    // 1 project, 1 plan
+    @Test
+    public void testGetCDResultWithOneProjectAndOnePlanBySize() {
+    	testWithEmptyBuildListBySize(1, 1, 1);
+    }
+    
+    // 5 projects, each with 1 plan
+    @Test
+    public void testGetCDResultWithSmallProjectSetAndOnePlanBySize() {
+    	testWithEmptyBuildListBySize(5, 1, 5);
+    }
+    
+    // 25 projects, each with 1 plan
+    @Test
+    public void testGetCDResultWithBiggerProjectSetAndOnePlanBySize() {
+    	testWithEmptyBuildListBySize(25, 1, 25);
+    }
+    
+    // 1 projects, each with 5 plans
+    @Test
+    public void testGetCDResultsWithOneProjectAndSmallPlanListBySize() {
+    	testWithEmptyBuildListBySize(1, 5, 5);
+    }
+    
+    // 4 projects, each with 5 plans
+    @Test
+    public void testGetCDResultsWithSmallProjectSetAndSmallPlanListBySize() {
+    	testWithEmptyBuildListBySize(4, 5, 20);
+    }
+    
+    // 20 projects, each with 5 plans
+    @Test
+    public void testGetCDResultsWithBiggerProjectSetAndSmallPlanListBySize() {
+    	testWithEmptyBuildListBySize(20, 5, 100);
+    }
+    
+    // 1 projects, each with 30 plans
+    @Test
+    public void testGetCDResultsWithOneProjectAndBiggerPlanListBySize() {
+    	testWithEmptyBuildListBySize(1, 30, 30);
+    }
+    
+    // 5 projects, each with 30 plans
+    @Test
+    public void testGetCDResultsWithSmallProjectSetAndBiggerPlanListBySize() {
+    	testWithEmptyBuildListBySize(5, 30, 150);
+    }
+    
+    // 20 projects, each with 30 plans
+    @Test
+    public void testGetCDResultsWithBiggerProjectSetAndBiggerPlanListBySize() {
+    	testWithEmptyBuildListBySize(20, 30, 600);
     }
     
     
     // ========== Private Helper Methods =========
     
-    // create a set of 2 projects
-    private Set<Project> createSmallProjectSet() {
-    	Project project1 = mock(Project.class);
-    	Project project2 = mock(Project.class);
-    	
-    	when(project1.getName()).thenReturn("Project 1");
-        when(project2.getName()).thenReturn("Project 2");
-        
-        Set<Project> projects = new HashSet<Project>();
-        projects.add(project1);
-        projects.add(project2);
+    // test for different project-plan sizes with empty build list
+    private void testWithEmptyBuildListBySize(int numProjects, int numPlans, int expected) {
+    	Set<Project> projects = createNProjects(numProjects);
+    	createProjectManager(projects);
+    	Map<Project, List<TopLevelPlan>> map = new HashMap<Project, List<TopLevelPlan>>();
+    	for (Project pj : projects) {
+    		List<TopLevelPlan> plans = createSmallPlanList(pj, numPlans);
+    		map.put(pj, plans);
+    	}
+    	createPlanManager(projects, map);
+    	createEmptyResultsSummaryManager(map);
+    	main = new MainManager(projectMgr, planMgr, resultsSumMgr);
+    	assertEquals("The count of CDResult list does not match", expected, main.getCDResults().size());
+    }
+    
+    // create a set of N projects
+    private Set<Project> createNProjects(int N) {
+    	Set<Project> projects = new HashSet<Project>();
+    	for (int i = 0; i < N; i++) {
+    		Project p = mock(Project.class);
+    		when(p.getName()).thenReturn("Project " + i);
+    		projects.add(p);
+    	}
         
         return projects;
     }
@@ -99,22 +155,20 @@ public class MainManagerTest {
     	when(projectMgr.getAllProjects()).thenReturn(projects);
     }
     
-    // create a list of 2 plans for the given project
-    private List<TopLevelPlan> createSmallPlanList(Project project) {
-    	TopLevelPlan plan1 = mock(TopLevelPlan.class);
-    	TopLevelPlan plan2 = mock(TopLevelPlan.class);
-    	
-    	String projName = project.getName();
-    	when(plan1.getName()).thenReturn(projName + " - Plan 1");
-    	when(plan2.getName()).thenReturn(projName + " - Plan 2");
-    	
+    // create a list of N plans for the given project
+    private List<TopLevelPlan> createSmallPlanList(Project project, int N) {
     	List<TopLevelPlan> plans = new ArrayList<TopLevelPlan>();
-    	plans.add(plan1);
-    	plans.add(plan2);
+		String projName = project.getName();
+    	for (int i = 0; i < N; i++) {
+    		TopLevelPlan plan = mock(TopLevelPlan.class);
+        	when(plan.getName()).thenReturn(projName + " - Plan " + i);
+        	plans.add(plan);
+    	}
     	
     	return plans;
     }
     
+    // create the planMgr for the given project-plan-map
     private void createPlanManager(Set<Project> projs, Map<Project, List<TopLevelPlan>> map) {
     	planMgr = mock(PlanManager.class);
     	for (Project p : projs) {
@@ -122,6 +176,7 @@ public class MainManagerTest {
     	}
     }
     
+    // create the resultsSumMgr on the given project-plan-map that returns empty build lists
     private void createEmptyResultsSummaryManager(Map<Project, List<TopLevelPlan>> map) {
     	resultsSumMgr = mock(ResultsSummaryManager.class);
     	for (Project pj : map.keySet()) {
