a5a1cd9681cb185881bb0204190a3a7b14be6468
Bessie <songy@cobalt.com>
2014-06-27 11:04:33 -0700

    Add not yet implemented test files for CDResultFactory.

---
+++ src/test/java/com/cobalt/cdpipeline/cdresult/SetLastDeploymentInfoTest.java
@@ -0,0 +1,13 @@
+package com.cobalt.cdpipeline.cdresult;
+
+import org.junit.Test;
+
+
+public class SetLastDeploymentInfoTest {
+
+	@Test
+	public void testSetLastDeploymentInfo() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
