444906cff2e75ccd6eceb2e385fd55ca5f16087c
ArielLin <lintz@cobalt.com>
2014-06-30 14:12:27 -0700

    Modified and added comments to CDResultFactory constructor. Added test for constructor in CDResultFactoryTest.

--- src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
+++ src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
@@ -1,12 +1,36 @@
 package com.cobalt.cdpipeline.cdresult;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.junit.*;
+
+import com.atlassian.bamboo.resultsummary.ResultsSummary;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AddAllAuthorsInCommitsTest.class, PipelineStagesTest.class,
 		SetCurrentBuildInfoTest.class, SetLastDeploymentInfoTest.class })
 public class CDResultFactoryTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorWithNullArguments() {
+		CDResultFactory fac = new CDResultFactory(null, null, null);
+	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorWithInvalidPlanName() {
+		CDResultFactory fac = new CDResultFactory("project", "projectplan", new ArrayList<ResultsSummary>());
+	}
+	
+	@Test
+	public void testConstructorWithNormalArguments() {
+		CDResultFactory fac = new CDResultFactory("Project", "Project - Plan", new ArrayList<ResultsSummary>());
+		
+		assertEquals("Project name doesn't match.", "Project", fac.cdresult.getProjectName());
+		assertEquals("Plan name doesn't match.", "Plan", fac.cdresult.getPlanName());
+	}
 }
