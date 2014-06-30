d0b27ecd6b93fc79933c1424f53f41529db46e4f
Bessie <songy@cobalt.com>
2014-06-30 14:45:48 -0700

    Change everything to match CDResultFactory(project, plan, projectkey, plankey, buildlist) and CDResult(project, plan, projectkey, plankey)

--- src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
+++ src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
@@ -18,17 +18,17 @@ public class CDResultFactoryTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testConstructorWithNullArguments() {
-		CDResultFactory fac = new CDResultFactory(null, null, null);
+		CDResultFactory fac = new CDResultFactory(null, null, null, null, null);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testConstructorWithInvalidPlanName() {
-		CDResultFactory fac = new CDResultFactory("project", "projectplan", new ArrayList<ResultsSummary>());
+		CDResultFactory fac = new CDResultFactory("project", "projectplan", "projectKey", "planKey", new ArrayList<ResultsSummary>());
 	}
 	
 	@Test
 	public void testConstructorWithNormalArguments() {
-		CDResultFactory fac = new CDResultFactory("Project", "Project - Plan", new ArrayList<ResultsSummary>());
+		CDResultFactory fac = new CDResultFactory("Project", "Project - Plan", "project", "plan", new ArrayList<ResultsSummary>());
 		
 		assertEquals("Project name doesn't match.", "Project", fac.cdresult.getProjectName());
 		assertEquals("Plan name doesn't match.", "Plan", fac.cdresult.getPlanName());
