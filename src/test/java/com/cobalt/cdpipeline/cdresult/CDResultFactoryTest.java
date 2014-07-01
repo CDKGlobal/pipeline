7c35d4afd32b1525a0220e131767606b30d010e3
ArielLin <lintz@cobalt.com>
2014-07-01 09:52:25 -0700

    Fixed AddAllAuthorInCommitsTest, CDResultFactoryTest, and MainManager to accomodate the change in CDResultFactory.

--- src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
+++ src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
@@ -4,33 +4,27 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 import org.junit.*;
 
 import com.atlassian.bamboo.resultsummary.ResultsSummary;
 
-@RunWith(Suite.class)
-@SuiteClasses({ AddAllAuthorsInCommitsTest.class, PipelineStagesTest.class,
-		SetCurrentBuildInfoTest.class, SetLastDeploymentInfoTest.class })
 public class CDResultFactoryTest {
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testConstructorWithNullArguments() {
-		CDResultFactory fac = new CDResultFactory(null, null, null, null, null);
+	public void testCreateCDResultWithNullArguments() {
+		CDResultFactory.createCDResult(null, null, null, null, null);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testConstructorWithInvalidPlanName() {
-		CDResultFactory fac = new CDResultFactory("project", "projectplan", "projectKey", "planKey", new ArrayList<ResultsSummary>());
+		CDResultFactory.createCDResult("project", "projectplan", "projectKey", "planKey", new ArrayList<ResultsSummary>());
 	}
 	
 	@Test
 	public void testConstructorWithNormalArguments() {
-		CDResultFactory fac = new CDResultFactory("Project", "Project - Plan", "project", "plan", new ArrayList<ResultsSummary>());
+		CDResult cdresult = CDResultFactory.createCDResult("Project", "Project - Plan", "project", "plan", new ArrayList<ResultsSummary>());
 		
-		assertEquals("Project name doesn't match.", "Project", fac.cdresult.getProjectName());
-		assertEquals("Plan name doesn't match.", "Plan", fac.cdresult.getPlanName());
+		assertEquals("Project name doesn't match.", "Project", cdresult.getProjectName());
+		assertEquals("Plan name doesn't match.", "Plan", cdresult.getPlanName());
 	}
 }
