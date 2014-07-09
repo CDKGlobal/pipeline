d586dbc41f6b35ce0a4598973440763223c95333
liuch <liuch@cobalt.com>
2014-07-09 10:06:13 -0700

    Fixed AddAllAuthorsInCommitsTest, CDResultFactoryTest and SetLastDeploymentInfoTest to adjust to the exception in ContributorBuilder

--- src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
+++ src/test/java/com/cobalt/cdpipeline/cdresult/CDResultFactoryTest.java
@@ -1,16 +1,39 @@
 package com.cobalt.cdpipeline.cdresult;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.junit.*;
 
+import com.atlassian.applinks.api.ApplicationLink;
+import com.atlassian.bamboo.applinks.JiraApplinksService;
+import com.atlassian.bamboo.jira.rest.JiraRestService;
 import com.atlassian.bamboo.resultsummary.ResultsSummary;
 
 public class CDResultFactoryTest {
-	ContributorBuilder cb = new ContributorBuilder(null, null);
+	
+	//ContributorBuilder cb = new ContributorBuilder(null, null);
+	ContributorBuilder cb;
 
+	@Before
+	public void setup() {
+		// mockup JiraApplinksService
+		JiraApplinksService jiraApplinksService = mock(JiraApplinksService.class);		
+		Iterator<ApplicationLink> itr = (Iterator<ApplicationLink>) mock(Iterator.class);
+		when(itr.hasNext()).thenReturn(false);	
+		Iterable<ApplicationLink> iterable = (Iterable<ApplicationLink>) mock(Iterable.class);
+		when(iterable.iterator()).thenReturn(itr);	
+		when(jiraApplinksService.getJiraApplicationLinks()).thenReturn(iterable);
+		// mockup JiraRestService	
+		JiraRestService jiraRestService = mock(JiraRestService.class);	
+		
+		cb = new ContributorBuilder(jiraApplinksService, jiraRestService);	 
+	}
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void testCreateCDResultWithNullArguments() {
 		CDResultFactory.createCDResult(null, null, null, null, null, cb);
--- src/test/java/com/cobalt/cdpipeline/cdresult/SetLastDeploymentInfoTest.java
+++ src/test/java/com/cobalt/cdpipeline/cdresult/SetLastDeploymentInfoTest.java
@@ -3,14 +3,18 @@ package com.cobalt.cdpipeline.cdresult;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 import org.junit.*;
 
+import com.atlassian.applinks.api.ApplicationLink;
+import com.atlassian.bamboo.applinks.JiraApplinksService;
 import com.atlassian.bamboo.author.Author;
 import com.atlassian.bamboo.chains.ChainResultsSummary;
 import com.atlassian.bamboo.commit.Commit;
+import com.atlassian.bamboo.jira.rest.JiraRestService;
 import com.atlassian.bamboo.resultsummary.ResultsSummary;
 import com.google.common.collect.ImmutableList;
 
@@ -33,11 +37,22 @@ public class SetLastDeploymentInfoTest {
 	private ChainResultsSummary fail2;
 	private Date day1;
 	private Date day2;
-	ContributorBuilder cb = new ContributorBuilder(null, null);
+	ContributorBuilder cb;
 
 	
 	@Before
 	public void setup(){
+		// mockup JiraApplinksService
+		JiraApplinksService jiraApplinksService = mock(JiraApplinksService.class);		
+		Iterator<ApplicationLink> itr = (Iterator<ApplicationLink>) mock(Iterator.class);
+		when(itr.hasNext()).thenReturn(false);	
+		Iterable<ApplicationLink> iterable = (Iterable<ApplicationLink>) mock(Iterable.class);
+		when(iterable.iterator()).thenReturn(itr);	
+		when(jiraApplinksService.getJiraApplicationLinks()).thenReturn(iterable);
+		// mockup JiraRestService	
+		JiraRestService jiraRestService = mock(JiraRestService.class);	
+		
+		cb = new ContributorBuilder(jiraApplinksService, jiraRestService);	 
 		day1 = new Date();
 		day2 = new Date();
 		day1.setDate(day1.getDate() - 1);
