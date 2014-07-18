6320142c20d597d2b5b8350a9705c27d13618486
Bessie <songy@cobalt.com>
2014-07-18 12:05:57 -0700

    Put results with no build to the bottom of the list.

--- src/main/resources/js/boardcontroller.js
+++ src/main/resources/js/boardcontroller.js
@@ -21,3 +21,16 @@ plugin.factory('autoRefresh', function ($http, $timeout) {
 		data: data
 	};
 });
+
+plugin.filter("emptyToEnd", function () {
+	return function (array, key) {
+		if(!angular.isArray(array)) return;
+        var present = array.filter(function (item) {
+            return item[key];
+        });
+        var empty = array.filter(function (item) {
+            return !item[key]
+        });
+		return present.concat(empty);
+	};
+});
