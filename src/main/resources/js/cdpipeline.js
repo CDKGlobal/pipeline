1863ed384a2738494c1c1aa10c9eb70180b6eb16
liuch <liuch@cobalt.com>
2014-07-23 11:29:43 -0700

    Fixed the CSS code and id/class names in vm file

--- src/main/resources/js/cdpipeline.js
+++ src/main/resources/js/cdpipeline.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function() {
 	var duration = 500;
 	jQuery(window).scroll(function() {
 		if (jQuery(this).scrollTop() > 0) {
-			jQuery('.back-to-top').fadeIn(duration);
+			jQuery('#scroll-to-top').fadeIn(duration);
 		} else {
-			jQuery('.back-to-top').fadeOut(duration);
+			jQuery('#scroll-to-top').fadeOut(duration);
 		}
 	});
 				
