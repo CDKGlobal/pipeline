d0d918cf33e9377ae8e4b97d4a4bce2c7477a497
ArielLin <lintz@cobalt.com>
2014-07-25 09:17:50 -0700

    Added the Queued-icon for the build state, instead of displaying not built (while having the first stage in progress).

--- src/main/java/com/cobalt/cdpipeline/cdresult/CDPipelineState.java
+++ src/main/java/com/cobalt/cdpipeline/cdresult/CDPipelineState.java
@@ -4,5 +4,5 @@ package com.cobalt.cdpipeline.cdresult;
  * Customized states that only represent states concerned by the pipeline view.
  */
 public enum CDPipelineState {
-	CD_SUCCESS, CD_FAILED, CD_IN_PROGRESS, CD_NOT_BUILT, CD_MANUALLY_PAUSED
+	CD_SUCCESS, CD_FAILED, CD_IN_PROGRESS, CD_NOT_BUILT, CD_MANUALLY_PAUSED, CD_QUEUED
 }
