c2009d62621d51d4f78ce14209dc91b5dbb50460
wilbert <lamwi@cobalt.com>
2014-07-28 16:09:50 -0700

    removed non-working fields

--- src/main/resources/js/maincontroller.js
+++ src/main/resources/js/maincontroller.js
@@ -1,11 +1,11 @@
 plugin.controller("MainController", function ($scope, $http) {
 	$scope.projectName = "CDPipeline";
-	$rootScope.fullscreen = false;
+
 
 	//toggles the button icon from expand to minify,
 	//fades the header
 	$scope.toggleHeader = function() {
-		$rootScope.fullscreen = !$rootScope.fullscreen;
+
 		AJS.$("#fullscreenIcon").toggleClass("fa-compress");
 		AJS.$.fn.slideFadeToggle  = function(speed, easing, callback) {
         	return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
