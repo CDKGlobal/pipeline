e9b1d9c90c81200be8fad0f364d8f315089e5662
Bessie <songy@cobalt.com>
2014-08-01 11:56:54 -0700

    Change the popup window back to bootstrap one.

--- src/main/resources/js/modalcontroller.js
+++ src/main/resources/js/modalcontroller.js
@@ -2,59 +2,56 @@
 plugin.controller("ModalController", function ($scope, $modal, $window) {
   
   /* --------------------------------------------------------------------------------------- */
+  /* -------------------------- Modal Window with bamboo content --------------------------- */
+  /* --------------------------------------------------------------------------------------- */
+  $scope.modalOpenUrl = function (url) {
+
+    if($scope.isFullScreen) {
+      $scope.url = url;
+      var modalInstance = $modal.open({
+        templateUrl: "Details.html",
+        controller: ModalInstanceCtrl,
+        size: "lg",
+        resolve: {
+          url: function () {
+            return $scope.url;
+          },
+          result: function () {
+            return "";
+          }
+        }
+      });
+    }
+    else {
+      $window.location.assign(url);
+    }
+  };
+
+  /* --------------------------------------------------------------------------------------- */
   /* -------------------------- Modal Window with pipeline content ------------------------- */
   /* --------------------------------------------------------------------------------------- */
-  $scope.modalOpen = function (size, result, url, modalContent) {
-
-  	$scope.url = url;
-  	$scope.result = result;
+  $scope.modalOpenContent = function (size, result, modalContent) {
+    $scope.result = result;
     var modalInstance = $modal.open({
       templateUrl: modalContent,
       controller: ModalInstanceCtrl,
       size: size,
       resolve: {
         url: function () {
-          return $scope.url;
+          return null;
         },
         result: function () {
-        	return $scope.result;
+          return $scope.result;
         }
       }
     });
   };
 
   /* --------------------------------------------------------------------------------------- */
-  /* -------------------------- Modal Window with bamboo content --------------------------- */
-  /* --------------------------------------------------------------------------------------- */
-  $scope.isFullScreen = false;
-  $scope.url = '';
-  var showWindow = false;
-  
-  // Sets whether to show the window
-  $scope.setShowWindow = function(val) {
-      showWindow = val;
-  };
-
-  // Returns whether to show the window
-  $scope.getShowWindow = function() {
-      return showWindow;
-  };
-
-  // Sets the current url to be the given one. 
-  // Sets a new window to open on full screen
-  $scope.setupModal = function (url) {
-      $scope.url = url;
-      if($scope.isFullScreen) {
-          $scope.setShowWindow(true);
-      }
-      else {
-          $window.location.assign($scope.url);
-      }
-  };
-
-  /* --------------------------------------------------------------------------------------- */
   /* ----------------------------------- Hide/Show Header ---------------------------------- */
   /* --------------------------------------------------------------------------------------- */
+    $scope.isFullScreen = false;
+  
   //toggles the button icon from expand to minify,
   //fades the header
   $scope.toggleHeader = function() {
