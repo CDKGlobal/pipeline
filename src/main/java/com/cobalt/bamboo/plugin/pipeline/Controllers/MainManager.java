0750a995db577843c567649ef52e6a46a1843e45
ArielLin <lintz@cobalt.com>
2014-08-05 18:04:59 -0700

    Added new Rest end point for getting list of changes

--- src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/MainManager.java
+++ src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/MainManager.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.atlassian.bamboo.applinks.JiraApplinksService;
+import com.atlassian.bamboo.author.Author;
+import com.atlassian.bamboo.chains.ChainResultsSummary;
+import com.atlassian.bamboo.commit.Commit;
+import com.atlassian.bamboo.commit.CommitFile;
 import com.atlassian.bamboo.plan.Plan;
 import com.atlassian.bamboo.plan.PlanExecutionManager;
+import com.atlassian.bamboo.plan.PlanKey;
+import com.atlassian.bamboo.plan.PlanKeys;
 import com.atlassian.bamboo.plan.PlanManager;
 import com.atlassian.bamboo.plan.TopLevelPlan;
 import com.atlassian.bamboo.project.Project;
@@ -13,6 +19,8 @@ import com.atlassian.bamboo.resultsummary.ResultsSummary;
 import com.atlassian.bamboo.resultsummary.ResultsSummaryManager;
 import com.cobalt.bamboo.plugin.pipeline.cdresult.CDResult;
 import com.cobalt.bamboo.plugin.pipeline.cdresult.CDResultFactory;
+import com.cobalt.bamboo.plugin.pipeline.cdresult.Change;
+import com.cobalt.bamboo.plugin.pipeline.cdresult.Contributor;
 import com.cobalt.bamboo.plugin.pipeline.cdresult.ContributorBuilder;
 
 /**
@@ -60,21 +68,79 @@ public class MainManager {
 		List<CDResult> resultList = new ArrayList<CDResult>();
 		
 		List<TopLevelPlan> plans = planManager.getAllPlans(TopLevelPlan.class);
-			for (Plan plan : plans) {
-				String planName = plan.getName();
-				String planKey = plan.getKey();
-				
-				Project project = plan.getProject();
-				String projectName = project.getName();
-				String projectKey = project.getKey();
-				
-				List<ResultsSummary> buildList = resultsSummaryManager.getResultSummariesForPlan(plan, 0, 0);
-				
-				CDResult result = CDResultFactory.createCDResult(projectName, planName, projectKey, planKey, 
-																buildList, contributorBuilder, planExecutionManager);
-				resultList.add(result);
-			}
+		for (Plan plan : plans) {
+			String planName = plan.getName();
+			String planKey = plan.getKey();
+			
+			Project project = plan.getProject();
+			String projectName = project.getName();
+			String projectKey = project.getKey();
+			
+			List<ResultsSummary> buildList = resultsSummaryManager.getResultSummariesForPlan(plan, 0, 0);
+			
+			CDResult result = CDResultFactory.createCDResult(projectName, planName, projectKey, planKey, 
+															buildList, contributorBuilder, planExecutionManager);
+			resultList.add(result);
+		}
 		
 		return resultList;
+	}
+	
+	public List<Change> getChangeListForPlan(String planKey) {
+		List<Change> changeList = new ArrayList<Change>();
+		
+		PlanKey planKeyObj = PlanKeys.getPlanKey(planKey); 
+		Plan plan = planManager.getPlanByKey(planKeyObj);
+		if (plan == null) {
+			return null;
+		}
+		List<ResultsSummary> buildList = resultsSummaryManager.getResultSummariesForPlan(plan, 0, 0);
+		if (buildList != null && buildList.size() > 0) {
+			addChangesToList(changeList, buildList.get(0).getCommits(), buildList.get(0).getBuildNumber());
+			
+			for (int i = 1; i < buildList.size(); i++) {
+				ChainResultsSummary currentBuild = (ChainResultsSummary) buildList.get(i);
+				
+				if (!currentBuild.isContinuable() && currentBuild.isSuccessful()) {
+					break;
+				}
+				
+				addChangesToList(changeList, buildList.get(i).getCommits(), currentBuild.getBuildNumber());
+			}
+		}
+		
+		return changeList;
+	}
+	
+	private void addChangesToList(List<Change> changeList, List<Commit> commits, int buildNumber) {
+		for (Commit commit : commits) {
+			Author author = commit.getAuthor();
+			String username = author.getLinkedUserName();
+			if (username == null) {
+				username = author.getName();
+			}
+			
+			Contributor contributor = contributorBuilder.createContributor(username, commit.getDate(), author.getFullName());
+			
+			// a list of changes
+			String comment = commit.getComment();
+			int importInfo = comment.indexOf("Imported from Git"); // TODO too specific
+			if (importInfo != -1) {
+				comment = comment.substring(0, importInfo);
+			}
+			
+			List<String> files = new ArrayList<String>();
+			List<CommitFile> commitFiles = commit.getFiles();
+			String revisionNum = "";
+			for (CommitFile commitFile : commitFiles) {
+				String filename = commitFile.getCleanName();
+				files.add(filename.substring(filename.lastIndexOf("/") + 1)); // remove the path
+				revisionNum = commitFile.getRevision();			
+			}
+			
+			Change change = new Change(author.getFullName(), contributor.getPictureUrl(), buildNumber, comment, commit.getDate(), files, revisionNum);
+			
+			changeList.add(change);
+		}
 	}
 }
