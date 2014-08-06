32393c991f1a5aba386a5826f931a276ea25df21
Bessie <songy@cobalt.com>
2014-08-06 12:09:31 -0700

    Create empty CDPerformanceFactory and ChangeListFactoryTest file...

---
+++ src/main/java/com/cobalt/bamboo/plugin/pipeline/cdperformance/CDPerformanceFactory.java
@@ -0,0 +1,13 @@
+package com.cobalt.bamboo.plugin.pipeline.cdperformance;
+
+import java.util.List;
+
+import com.atlassian.bamboo.resultsummary.ResultsSummary;
+import com.cobalt.bamboo.plugin.pipeline.cdresult.ContributorBuilder;
+
+public class CDPerformanceFactory {
+	
+	public static CDPerformance createCDPerformance(List<ResultsSummary> buildList, ContributorBuilder contributorBuilder){
+		return null;
+	}
+}
