dc94865653fde06bcdccc4ad125ce0d3c9fa0138
ArielLin <lintz@cobalt.com>
2014-08-12 15:33:43 -0700

    Implemented caching, including first load when cache is empty and cache updating when events happen. Redirected the front-end to read from the cache.

--- src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/BuildActivityListener.java
+++ src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/BuildActivityListener.java
@@ -1,20 +1,9 @@
 package com.cobalt.bamboo.plugin.pipeline.Controllers;
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-
-import com.atlassian.bamboo.builder.BuildState;
-import com.atlassian.bamboo.builder.LifeCycleState;
-import com.atlassian.bamboo.event.BuildFinishedEvent;
 import com.atlassian.bamboo.event.ChainCompletedEvent;
 import com.atlassian.bamboo.event.HibernateEventListener;
 import com.atlassian.bamboo.event.StageCompletedEvent;
-import com.atlassian.bamboo.plan.PlanKey;
 import com.atlassian.bamboo.v2.build.events.BuildQueuedEvent;
-import com.atlassian.bamboo.v2.build.events.BuildTriggeredEvent;
 import com.atlassian.event.Event;
 import com.cobalt.bamboo.plugin.pipeline.customevent.BuildStartedEvent;
 
@@ -23,98 +12,54 @@ import com.cobalt.bamboo.plugin.pipeline.customevent.BuildStartedEvent;
  * started, finished, stage completed, and plan completed.
  */
 public class BuildActivityListener implements HibernateEventListener {
-
+	private CacheManager cacheManager;
+	
+	public BuildActivityListener(CacheManager cacheManager) {
+		this.cacheManager = cacheManager;
+	}
+	
 	@Override
 	public Class[] getHandledEventClasses() {
-		return new Class[]{BuildTriggeredEvent.class, BuildQueuedEvent.class, 
-							BuildStartedEvent.class, BuildFinishedEvent.class, 
+		return new Class[]{BuildQueuedEvent.class, BuildStartedEvent.class, 
 							StageCompletedEvent.class, ChainCompletedEvent.class};
 	}
 
 	@Override
 	public void handleEvent(Event event) {
-		// TODO: Get the plankey and update that plan in the cache.
 		
-		/* Do NOT delete for now. - 08/08/2014
-		 * TODO
-		 * For debugging pusposes, print event details to a file.*/
-		String s = "";
-		
-		if (event instanceof BuildTriggeredEvent) {
-			BuildTriggeredEvent e = (BuildTriggeredEvent) event;
-			
-			PlanKey pk = e.getPlanKey();
-			
-			s += "\n[[ BuildTiggeredEvent ]]\n";
-			s += "Plankey: " + pk + "\n";
-			s += "Partial PlanKey: " + pk.getPartialKey() + "\n";
-			s += "Plan Result Key: " + e.getPlanResultKey() + "\n";
-		} else if (event instanceof BuildQueuedEvent) {
+		if (event instanceof BuildQueuedEvent) {
 			BuildQueuedEvent e = (BuildQueuedEvent) event;
 			
-			PlanKey pk = e.getPlanKey();
+			// the given plan key is in the format of "PROJECT-PLAN-JOB"
+			// need to strip to just "PROJECT-PLAN"
+			String givenPlanKey = e.getPlanKey().getKey();
+			int endIndex = givenPlanKey.lastIndexOf("-");
+			final String planKey = givenPlanKey.substring(0, endIndex);
 			
-			s += "\n[[ BuildQueuedEvent ]]\n";
-			s += "Plankey: " + pk + "\n";
-			s += "Partial PlanKey: " + pk.getPartialKey() + "\n";
-			s += "Plan Result Key: " + e.getPlanResultKey() + "\n";
+			cacheManager.updateCDResultForPlan(planKey);
+			
 		} else if (event instanceof BuildStartedEvent) {
 			BuildStartedEvent e = (BuildStartedEvent) event;
 			
-			LifeCycleState ls = e.getLifeCycleState(); 
-			BuildState state = e.getBuildState();
-			int buildNum = e.getBuildNumber();
-			PlanKey pk = e.getPlanKey();
+			// the given plan key is in the format of "PROJECT-PLAN-JOB"
+			// need to strip to just "PROJECT-PLAN"
+			String givenPlanKey = e.getPlanKey().getKey();
+			int endIndex = givenPlanKey.lastIndexOf("-");
+			final String planKey = givenPlanKey.substring(0, endIndex);
 
-			s += "\n[[ BuildStartedEvent ]]\n";
-			s += "Plankey: " + pk;
-			s += "Partial PlanKey: " + pk.getPartialKey() + "\n";
-			s += "Plan Result Key: " + e.getPlanResultKey() + "\n";
-			s += "\n Build #: " + buildNum;
-			s += "\n LifeCycleState: " + ls;
-			s += "\n Build state: " + state + "\n";
-		} else if (event instanceof BuildFinishedEvent) {
-			BuildFinishedEvent e = (BuildFinishedEvent) event;
-
-			LifeCycleState ls = e.getLifeCycleState(); 
-			BuildState state = e.getBuildState();
-			int buildNum = e.getBuildNumber();
-			PlanKey pk = e.getPlanKey();
-
-			s += "\n[[ BuildFinishedEvent ]]\n";
-			s += "Plankey: " + pk;
-			s += "Partial PlanKey: " + pk.getPartialKey() + "\n";
-			s += "Plan Result Key: " + e.getPlanResultKey() + "\n";
-			s += "\n Build #: " + buildNum;
-			s += "\n LifeCycleState: " + ls;
-			s += "\n Build state: " + state + "\n";
+			cacheManager.updateCDResultForPlan(planKey);
+			
 		} else if (event instanceof StageCompletedEvent) {
 			StageCompletedEvent e = (StageCompletedEvent) event;
 			
-			PlanKey pk = e.getPlanKey();
+			// the given plan key is in the right format (PROJECT-PLAN)
+			cacheManager.updateCDResultForPlan(e.getPlanKey().getKey());
 			
-			s += "\n[[ StageCompletedEvent ]]\n";
-			s += "Plankey: " + pk + "\n";
-			s += "Partial PlanKey: " + pk.getPartialKey() + "\n";
-			s += "Plan Result Key: " + e.getPlanResultKey() + "\n";
 		} else if (event instanceof ChainCompletedEvent) {
 			ChainCompletedEvent e = (ChainCompletedEvent) event;
 			
-			PlanKey pk = e.getPlanKey();
-			
-			s += "\n[[ ChainCompletedEvent ]]\n";
-			s += "Plankey: " + pk + "\n";
-			s += "Partial PlanKey: " + pk.getPartialKey() + "\n";
-			s += "Plan Result Key: " + e.getPlanResultKey() + "\n";
-		}
-			
-		try {
-			FileWriter fw = new FileWriter("../../../test-build-activity-result.txt", true);
-			fw.write(s);
-			fw.close();
-		} catch (IOException e2) {
-			e2.printStackTrace();
+			// the given plan key is in the right format (PROJECT-PLAN)
+			cacheManager.updateCDResultForPlan(e.getPlanKey().getKey());
 		}
 	}
-
 }
--- src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/CacheManagerImpl.java
+++ src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/CacheManagerImpl.java
@@ -1,47 +1,116 @@
 package com.cobalt.bamboo.plugin.pipeline.Controllers;
 
 import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import com.atlassian.bamboo.plan.PlanKey;
+import com.atlassian.sal.api.transaction.TransactionCallback;
+import com.atlassian.sal.api.transaction.TransactionTemplate;
+import com.cobalt.bamboo.plugin.pipeline.cache.CDResultsCache;
 import com.cobalt.bamboo.plugin.pipeline.cdresult.CDResult;
 
 public class CacheManagerImpl implements CacheManager {
 	private final MainManager mainManager;
-	// TODO cache field
+	private TransactionTemplate transactionTemplate;
+	private ReadWriteLock lock;
+	CDResultsCache cdResultsCache;
 	
-	public CacheManagerImpl(MainManager mainManager){
+	/**
+	 * Constructs a CacheManager object.
+	 * 
+	 * @param mainManager The MainManager to get CDResults from.
+	 * @param transactionTemplate The TransactionTemplate to allow access to the Bamboo
+	 *                            database from outside of a web request.
+	 */
+	public CacheManagerImpl(MainManager mainManager, TransactionTemplate transactionTemplate){
 		this.mainManager = mainManager;
-		
-		// put all data from mainManager.getCDResult() into the cache (first load)
-	}
-	
-	@Override
-	public List<CDResult> getCDResults() { // TODO: change it!
-		return mainManager.getCDResults();
+		this.transactionTemplate = transactionTemplate;
+		lock = new ReentrantReadWriteLock();
+		cdResultsCache = new CDResultsCache();
 	}
 
 	@Override
 	public void putAllCDResult() {
-		// TODO Auto-generated method stub
+		lock.writeLock().lock();
+		transactionTemplate.execute(new TransactionCallback() {
+			
+			@Override
+			public Object doInTransaction() {
+				unlockedCacheRefresh();
+				return null;
+			}
+		});
 		
+		lock.writeLock().unlock();
 	}
-
+	
 	@Override
-	public void putCDResultForPlan(PlanKey planKey, CDResult cdresult) {
-		// TODO Auto-generated method stub
+	public void updateCDResultForPlan(final String planKey) {
+		lock.writeLock().lock();
 		
+		if (cdResultsCache.isEmpty()) {
+			transactionTemplate.execute(new TransactionCallback() {
+				
+				@Override
+				public Object doInTransaction() {
+					unlockedCacheRefresh();
+					return null;
+				}
+			});
+		} else {
+			transactionTemplate.execute(new TransactionCallback() {
+				
+				@Override
+				public Object doInTransaction() {
+					CDResult cdResult = mainManager.getCDResultForPlan(planKey);
+					if (cdResult != null) {
+						cdResultsCache.put(planKey, cdResult);
+					}
+					return null;
+				}
+			});
+		}
+		
+		lock.writeLock().unlock();
 	}
 
 	@Override
 	public List<CDResult> getAllCDResult() {
-		// TODO Auto-generated method stub
-		return null;
+		// to avoid write-lock first causing reading to be waiting for each other
+		lock.readLock().lock();
+		if(!cdResultsCache.isEmpty()){
+			List<CDResult> results = cdResultsCache.getAllCDResults();
+			lock.readLock().unlock();
+			return results;
+		}
+		lock.readLock().unlock();
+		
+		lock.writeLock().lock();
+		if (cdResultsCache.isEmpty()) {
+			unlockedCacheRefresh();
+		}
+		lock.writeLock().unlock();
+
+		lock.readLock().lock();
+		List<CDResult> results = cdResultsCache.getAllCDResults();
+		lock.readLock().unlock();
+		
+		return results;
 	}
 
 	@Override
 	public void clearCache() {
-		// TODO Auto-generated method stub
-		
+		lock.writeLock().lock();
+		cdResultsCache.clear();
+		lock.writeLock().unlock();
 	}
 
+	private void unlockedCacheRefresh() {
+		// create a new temporary cache to avoid locking the cache for too long
+		this.cdResultsCache.clear();
+		List<CDResult> cdResults = mainManager.getCDResults();
+		for (CDResult cdResult : cdResults) {
+			this.cdResultsCache.put(cdResult.getPlanKey(), cdResult);
+		}
+	}
 }
