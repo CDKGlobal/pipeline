2875860760996d34b2e6ba34cbc8e402130d1aba
ArielLin <lintz@cobalt.com>
2014-08-13 11:52:29 -0700

    Solved the dev instance Spring cannot find EventPublisher problem. Deleted BuildStartedEvent related files/code. Modify event listener to only listen to build queued and plan completed.

--- src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/BuildActivityListener.java
+++ src/main/java/com/cobalt/bamboo/plugin/pipeline/Controllers/BuildActivityListener.java
@@ -2,10 +2,8 @@ package com.cobalt.bamboo.plugin.pipeline.Controllers;
 
 import com.atlassian.bamboo.event.ChainCompletedEvent;
 import com.atlassian.bamboo.event.HibernateEventListener;
-import com.atlassian.bamboo.event.StageCompletedEvent;
 import com.atlassian.bamboo.v2.build.events.BuildQueuedEvent;
 import com.atlassian.event.Event;
-import com.cobalt.bamboo.plugin.pipeline.customevent.BuildStartedEvent;
 
 /**
  * A custom event listener that listens to build activities: triggered, queued, 
@@ -20,8 +18,7 @@ public class BuildActivityListener implements HibernateEventListener {
 	
 	@Override
 	public Class[] getHandledEventClasses() {
-		return new Class[]{BuildQueuedEvent.class, BuildStartedEvent.class, 
-							StageCompletedEvent.class, ChainCompletedEvent.class};
+		return new Class[]{BuildQueuedEvent.class, ChainCompletedEvent.class};
 	}
 
 	@Override
@@ -37,23 +34,6 @@ public class BuildActivityListener implements HibernateEventListener {
 			final String planKey = givenPlanKey.substring(0, endIndex);
 			
 			cacheManager.updateCDResultForPlan(planKey);
-			
-		} else if (event instanceof BuildStartedEvent) {
-			BuildStartedEvent e = (BuildStartedEvent) event;
-			
-			// the given plan key is in the format of "PROJECT-PLAN-JOB"
-			// need to strip to just "PROJECT-PLAN"
-			String givenPlanKey = e.getPlanKey().getKey();
-			int endIndex = givenPlanKey.lastIndexOf("-");
-			final String planKey = givenPlanKey.substring(0, endIndex);
-
-			cacheManager.updateCDResultForPlan(planKey);
-			
-		} else if (event instanceof StageCompletedEvent) {
-			StageCompletedEvent e = (StageCompletedEvent) event;
-			
-			// the given plan key is in the right format (PROJECT-PLAN)
-			cacheManager.updateCDResultForPlan(e.getPlanKey().getKey());
 			
 		} else if (event instanceof ChainCompletedEvent) {
 			ChainCompletedEvent e = (ChainCompletedEvent) event;
